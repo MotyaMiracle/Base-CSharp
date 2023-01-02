e3dbb0a8057dbeadb52ee81ed6987191dc3c1e32
MotyaMiracle <motyamiracle@gmail.com>
2023-01-02 14:28:01 +0300

    Done!

--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+                
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally // Блок можно опустить
+            {
+                Console.WriteLine("Блок finally");
+            }
+            Console.WriteLine("Конец программы");
+
+            Square("12");
+            Square("ab");
+        }
+
+        public static void Square(string data)
+        {
+            if(int.TryParse(data, out var x)) 
+            {
+                Console.WriteLine($"Квадрат числа {x}: {x * x}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+            
         }
     }
 }
