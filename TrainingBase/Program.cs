007fcfac8585388e95813b2e655c733552b366c2
MotyaMiracle <motyamiracle@gmail.com>
2023-01-04 20:22:29 +0300

    Done!

--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -4,49 +4,12 @@
     {
         async static Task Main(string[] args)
         {
-            await PrintAsync();   // вызов асинхронного метода
-            Console.WriteLine("Некоторые действия в методе Main");
-
-            await PrintNameAsync("Tom");
-            await PrintNameAsync("Bob");
-            await PrintNameAsync("Sam");
-
-            
-
-
-
-
-            void Print()
+            Repository repo = new Repository();
+            IAsyncEnumerable<string> data = repo.GetDataAsync();
+            await foreach (var name in data)
             {
-                Thread.Sleep(3000);     // имитация продолжительной работы
-                Console.WriteLine("Hello METANIT.COM");
-            }
-
-            // определение асинхронного метода
-            async Task PrintAsync()
-            {
-                await Task.Delay(3000); // имитация продолжительной работы
-                // или так
-                //await Task.Delay(TimeSpan.FromMilliseconds(3000));
-                Console.WriteLine("Начало метода PrintAsync"); // выполняется синхронно
-                await Task.Run(() => Print());                // выполняется асинхронно
-                Console.WriteLine("Конец метода PrintAsync");
-            }
-            // определение асинхронного метода
-            async Task PrintNameAsync(string name)
-            {
-                await Task.Delay(3000);     // имитация продолжительной работы
                 Console.WriteLine(name);
             }
-            // асинхронное лямбда-выражение
-            Func<string, Task> printer = async (message) =>
-            {
-                await Task.Delay(3000);
-                Console.WriteLine(message);
-            };
-
-            await printer("Hello World");
-            await printer("Hello METANIT.COM");
         }
     }
 }
---
+++ TrainingBase/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBase
+{
+    public class Repository
+    {
+        string[] data = { "Tom", "Sam", "Kate", "Alice", "Bob" };
+        public async IAsyncEnumerable<string> GetDataAsync()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine($"Получаем {i + 1} элемент");
+                await Task.Delay(500);
+                yield return data[i];
+            }
+        }
+    }
+}
