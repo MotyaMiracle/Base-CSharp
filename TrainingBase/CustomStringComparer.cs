93dbcde4680800ca709112f001beee68dd00e9eb
MotyaMiracle <motyamiracle@gmail.com>
2023-01-05 21:18:39 +0300

    Done!

---
+++ TrainingBase/CustomStringComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBase
+{
+    // сравнение по длине строки
+    public class CustomStringComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            int xLength = x?.Length ?? 0; // если x равно null, то длина 0
+            int yLength = y?.Length ?? 0;
+            return xLength - yLength;
+        }
+    }
+}
--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -4,31 +4,83 @@
     {
         static void Main(string[] args)
         {
-            string[] people = { "Tom", "Bob", "Sam", "Tim", "Thomas", "Bill" };
-
-            // создаем новый список для результатов
-            var selectedPeople = new List<string>();
-
-            // Проходим по массиву
-            foreach (string person in people)
-            {
-                // если строка начинается на букву T, добавляем в список
-                if (person.ToUpper().StartsWith("T"))
-                    selectedPeople.Add(person);
-            }
-
-            // Сортируем список
-            selectedPeople.Sort();
-
-            foreach (string person in selectedPeople)
-                Console.WriteLine(person);
-
+            int[] numbers = { 3, 12, 4, 10};
+            var orderedNumbers = from i in numbers orderby i select i;
+            foreach (int i in orderedNumbers)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            var orderedNumbers2 = numbers.OrderBy(i => i);
+            foreach (int i in orderedNumbers2)
+                Console.WriteLine(i);
             Console.WriteLine();
 
-            var selectedPeople2 = people.Where(p => p.ToUpper().StartsWith("T")).OrderBy(p => p);
-            foreach (string person in selectedPeople2)
-                Console.WriteLine(person);
+            string[] people = { "Tom", "Bob", "Sam" };
+            var orderedPeople = from p in people orderby p select p;
+            foreach (var p in orderedPeople)
+                Console.WriteLine(p);
+            Console.WriteLine();
+            var orderedPeople2 = people.OrderBy(p => p);
+            foreach (var p in orderedPeople2)
+                Console.WriteLine(p);
+            Console.WriteLine();
 
+            var people2 = new List<Person>
+            {
+                new Person("Tom", 37),
+                new Person("Sam", 28),
+                new Person("Tom", 22),
+                new Person("Bob", 41),
+            };
+            // с помощью оператора orderby
+            var sortedPeople1 = from p in people2
+                                orderby p.Name
+                                select p;
+            foreach (var p in sortedPeople1)
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            Console.WriteLine();
+            // с помощью метода OrderBy
+            var sortedPeople2 = people2.OrderBy(p => p.Name);
+
+            foreach (var p in sortedPeople2)
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            Console.WriteLine();
+            // с помощью оператора orderby descending
+            var orderedNumbers3 = from i in numbers
+                                 orderby i descending
+                                 select i;
+            foreach (int i in orderedNumbers3)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            // с помощью метода OrderByDescending
+            var orderedNumbers4 = numbers.OrderByDescending(n => n);
+            foreach (int i in orderedNumbers4)
+                Console.WriteLine(i);
+            Console.WriteLine();
+            // с помощью оператора orderby
+            var sortedPeople3 = from p in people2
+                                orderby p.Name, p.Age
+                                select p;
+            foreach (var p in sortedPeople3)
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            Console.WriteLine();
+            // с помощью оператора orderby
+            var sortedPeople4 = from p in people2
+                                orderby p.Name, p.Age descending  // сортировка по возрасту по убыванию
+                                select p;
+            foreach (var p in sortedPeople4)
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            Console.WriteLine();
+            // с помощью метода ThenByDescending
+            var sortedPeople5 = people2.OrderBy(p => p.Name).ThenByDescending(p => p.Age);
+            foreach (var p in sortedPeople5)
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            Console.WriteLine();
+
+            string[] people3 = new[] { "Kate", "Tom", "Sam", "Mike", "Alice" };
+            var sortedPeople = people3.OrderBy(p => p, new CustomStringComparer());
+
+            foreach (var p in sortedPeople)
+                Console.WriteLine(p);
         }
     }
 }
