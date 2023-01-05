f56630a3b87d94bf90f8a3d91e2424367f439c46
MotyaMiracle <motyamiracle@gmail.com>
2023-01-05 18:47:08 +0300

    Done!

--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace TrainingBase
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Thomas", "Bill" };
+
+            // создаем новый список для результатов
+            var selectedPeople = new List<string>();
+
+            // Проходим по массиву
+            foreach (string person in people)
+            {
+                // если строка начинается на букву T, добавляем в список
+                if (person.ToUpper().StartsWith("T"))
+                    selectedPeople.Add(person);
+            }
+
+            // Сортируем список
+            selectedPeople.Sort();
+
+            foreach (string person in selectedPeople)
+                Console.WriteLine(person);
+
+            Console.WriteLine();
+
+            var selectedPeople2 = people.Where(p => p.ToUpper().StartsWith("T")).OrderBy(p => p);
+            foreach (string person in selectedPeople2)
+                Console.WriteLine(person);
+
         }
     }
 }
