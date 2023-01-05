dd8b1f94b52b1f23bf47a66d578bffd5f256773a
MotyaMiracle <motyamiracle@gmail.com>
2023-01-05 21:26:29 +0300

    Done!

---
+++ TrainingBase/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBase
+{
+    public class Person
+    {
+         public string Name { get; }
+        public Person(string name) => Name = name;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Person person) return Name == person.Name;
+            return false;
+        }
+        public override int GetHashCode() => Name.GetHashCode();
+    }
+}
