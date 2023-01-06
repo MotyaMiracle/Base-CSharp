2bdb5ad3aef67835562c32e7cd4c088264659453
MotyaMiracle <motyamiracle@gmail.com>
2023-01-06 14:08:29 +0300

    Done!

---
+++ TrainingBase/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBase
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public string? Company { get; set; }
+        public Person(string name, string company) 
+        {
+            Name = name;
+            Company = company;
+        }
+    }
+}
--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -4,31 +4,66 @@
     {
         static void Main(string[] args)
         {
-            string[] people = { "Tom", "Bob", "Sam", "Tim", "Thomas", "Bill" };
-
-            // создаем новый список для результатов
-            var selectedPeople = new List<string>();
-
-            // Проходим по массиву
-            foreach (string person in people)
+            Person[] people =
             {
-                // если строка начинается на букву T, добавляем в список
-                if (person.ToUpper().StartsWith("T"))
-                    selectedPeople.Add(person);
+                new Person("Tom", "Microsoft"), new Person("Sam", "Google"),
+                new Person("Bob", "JetBrains"), new Person("Mike", "Microsoft"),
+                new Person("Kate", "JetBrains"), new Person("Alice", "Microsoft"),
+            };
+
+            //var companies = from person in people
+            //                group person by person.Company;
+
+            var companies = people.GroupBy(p => p.Company);
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+
+                foreach (var person in company)
+                {
+                    Console.WriteLine(person.Name);
+                }
+                Console.WriteLine(); // для разделения между группами
             }
 
-            // Сортируем список
-            selectedPeople.Sort();
+            // Создание нового объекта при группировке
+            //var companies1 = from person in people
+            //                group person by person.Company into g
+            //                select new { Name = g.Key, Count = g.Count() }; ;
 
-            foreach (string person in selectedPeople)
-                Console.WriteLine(person);
+            var companies1 = people.GroupBy(p => p.Company).Select(g => new { Name = g.Key, Count = g.Count() });
 
+            foreach (var company in companies1)
+            {
+                Console.WriteLine($"{company.Name} : {company.Count}");
+            }
             Console.WriteLine();
-
-            var selectedPeople2 = people.Where(p => p.ToUpper().StartsWith("T")).OrderBy(p => p);
-            foreach (string person in selectedPeople2)
-                Console.WriteLine(person);
-
+            // Вложенные запросы
+            //var companies2 = from person in people
+            //                group person by person.Company into g
+            //                select new
+            //                {
+            //                    Name = g.Key,
+            //                    Count = g.Count(),
+            //                    Employees = from p in g select p
+            //                };
+            var companies2 = people.GroupBy(p => p.Company).Select(g => new
+            {
+                Name = g.Key,
+                Count = g.Count(),
+                Employees = g.Select(p => p)
+            });
+            foreach (var company in companies2)
+            {
+                Console.WriteLine($"{company.Name} : {company.Count}");
+                foreach (var employee in company.Employees)
+                {
+                    Console.WriteLine(employee.Name);
+                }
+                Console.WriteLine(); // для разделения компаний
+            }
+            Console.WriteLine();
         }
     }
 }
