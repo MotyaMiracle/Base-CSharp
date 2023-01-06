ddf59a77e25131d08b6329bf0da776ad570bf116
MotyaMiracle <motyamiracle@gmail.com>
2023-01-06 14:37:10 +0300

    Done!

---
+++ TrainingBase/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBase
+{
+    public class Company
+    {
+        public string? Title { get; set; }
+        public string? Language { get; set; }
+        public Company(string title, string language)
+        {
+            Title = title;
+            Language = language;
+        }
+    }
+}
--- TrainingBase/Program.cs
+++ TrainingBase/Program.cs
@@ -4,30 +4,57 @@
     {
         static void Main(string[] args)
         {
-            string[] people = { "Tom", "Bob", "Sam", "Tim", "Thomas", "Bill" };
-
-            // создаем новый список для результатов
-            var selectedPeople = new List<string>();
-
-            // Проходим по массиву
-            foreach (string person in people)
+            Person[] people =
             {
-                // если строка начинается на букву T, добавляем в список
-                if (person.ToUpper().StartsWith("T"))
-                    selectedPeople.Add(person);
-            }
+                new Person("Tom", "Microsoft"), new Person("Sam", "Google"),
+                new Person("Bob", "JetBrains"), new Person("Mike", "Microsoft"),
+            };
+            Company[] companies =
+            {
+                new Company("Microsoft", "C#"),
+                new Company("Google", "Go"),
+                new Company("Oracle", "Java")
+            };
+            //var employees = from p in people
+            //                join c in companies on p.Company equals c.Title
+            //                select new { Name = p.Name, Company = c.Title, Language = c.Language };
+            var employees = people.Join(companies, p => p.Company, c => c.Title,
+                (p, c) => new { Name = p.Name, Company = c.Title, Language = c.Language });
 
-            // Сортируем список
-            selectedPeople.Sort();
-
-            foreach (string person in selectedPeople)
-                Console.WriteLine(person);
+            foreach (var emp in employees)
+                Console.WriteLine($"{emp.Name} - {emp.Company} ({emp.Language})");
 
             Console.WriteLine();
 
-            var selectedPeople2 = people.Where(p => p.ToUpper().StartsWith("T")).OrderBy(p => p);
-            foreach (string person in selectedPeople2)
-                Console.WriteLine(person);
+            var personnel = companies.GroupJoin(people, // второй набор
+                    c => c.Title, // свойство-селектор объекта из первого набора
+                    p => p.Company, // свойство-селектор объекта из второго набора
+                    (c, employees) => new   // результат
+             {
+                 Title = c.Title,
+                 Employees = employees
+             });
+            foreach (var company in personnel)
+            {
+                Console.WriteLine(company.Title);
+                foreach (var emp in company.Employees)
+                {
+                    Console.WriteLine(emp.Name);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            // Метод Zip()
+            var courses = new List<Course> { new Course("C#"), new Course("Java") };
+            var students = new List<Student> { new Student("Tom"), new Student("Bob") };
+
+            var enrollments = courses.Zip(students);
+            foreach (var enrollment in enrollments)
+            {
+                Console.WriteLine($"{enrollment.First.Title} - {enrollment.Second.Name}");
+            }
 
         }
     }
